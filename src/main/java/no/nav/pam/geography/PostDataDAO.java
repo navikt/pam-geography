84761d739ee8635cb00666bfcbe688779ef58edb
Aasmund Nordstoga <aasmund.nordstoga@nav.no>
2019-12-10 13:16:23 +0100

    KOR2020: La til fylke -> postnr mapping
    Endra til å gå ut fra eksisterende map per Øyvinds forslag i PR.

--- src/main/java/no/nav/pam/geography/PostDataDAO.java
+++ src/main/java/no/nav/pam/geography/PostDataDAO.java
@@ -17,12 +17,10 @@ public class PostDataDAO {
     private final static String FILENAME = "postal_codes_no.tsv";
 
     private final Map<String, PostData> postalCodeTable;
-    private final Map<String, List<String>> countyTable;
 
     public PostDataDAO() throws IOException {
 
         postalCodeTable = new HashMap<>();
-        countyTable = new HashMap<>();
 
         String line;
         String csvSplitBy = "\t";
@@ -40,12 +38,6 @@ public class PostDataDAO {
                 if (municipality != null && county != null && postalCode != null && city !=null) {
                     PostData data = new PostData(postalCode, city, municipality, county);
                     postalCodeTable.put(data.getPostalCode(), data);
-
-                    if (countyTable.containsKey(county.getCode())) {
-                        countyTable.get(county.getCode()).add(postalCode);
-                    } else {
-                        countyTable.put(county.getCode(), new ArrayList<>(Arrays.asList(postalCode)));
-                    }
                 } else {
                     throw new IOException("There was an error parsing post data from file for postal code {}");
                 }
@@ -57,11 +49,10 @@ public class PostDataDAO {
         return Optional.ofNullable(postalCodeTable.get(postalCode));
     }
 
-    public List<String> findPostalCodes(String countyCode) {
-        if (!countyTable.containsKey(countyCode)) {
-            return Collections.emptyList();
-        }
-        return countyTable.get(countyCode);
+    public List<PostData> postDataForCounty(String countyCode) {
+        return postalCodeTable.values()
+                .stream().filter(p -> p.getCounty().getCode().equals(countyCode))
+                .collect(Collectors.toList());
     }
 
     public List<PostData> getAllPostData() {
